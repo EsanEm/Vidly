ae2b8e6b89a1397f3c2b0399c1742417a3605232
EsanEm <62729091+EsanEm@users.noreply.github.com>
2020-05-06 17:45:35 +0100

    Section 3-3 Solution - Display the list of movies and their details using data in the database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,19 +5,33 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
+
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
 
+        // To connect to database
+        private ApplicationDbContext _context;
 
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // End of connect to database
 
 
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
@@ -26,7 +40,7 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
@@ -38,14 +52,14 @@ namespace Vidly.Controllers
 
 
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>
+        //    {
+        //        new Movie { Id = 1, Name = "Shrek" },
+        //        new Movie { Id = 2, Name = "Wall-e" }
+        //    };
+        //}
 
 
 
@@ -61,25 +75,25 @@ namespace Vidly.Controllers
 
 
 
-        // GET: Movies
-        public ActionResult Random()
-        {
+        //// GET: Movies
+        //public ActionResult Random()
+        //{
 
-            var movie = new Movie() { Name = "Shrek"};
+        //    var movie = new Movie() { Name = "Shrek"};
             
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Esan"},
-                new Customer {Name = "Massa"},
-            };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Esan"},
+        //        new Customer {Name = "Massa"},
+        //    };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
             
-            return View(viewModel);
-        }
+        //    return View(viewModel);
+        //}
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,22 @@ namespace Vidly.Models
     {
 
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public byte NumberInStock { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public byte GenreId { get; set; }   
 
 
 
