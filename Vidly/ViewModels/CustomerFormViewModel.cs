8d1493de754c5448098828be9a7c1f6af1a5b804
EsanEm <62729091+EsanEm@users.noreply.github.com>
2020-05-07 15:22:08 +0100

    Add a form to add/update a customer

---
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using Microsoft.Ajax.Utilities;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+
+        public Customer Customer { get; set; }
+
+        
+    }
+}
